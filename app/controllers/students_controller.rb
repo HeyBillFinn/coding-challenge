d27b62a94c29c7cdd24f94eba9798318ba36329c
Bill <william.finn@gmail.com>
2015-08-29 14:08:44 -0500

    Initial framework.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,20 @@
 class StudentsController < ApplicationController
   def new
   end
+
+  def create
+    @student = Student.new(article_params)
+
+    @student.save
+    redirect_to @student
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  private
+    def article_params
+      params.require(:student).permit(:name, :email_address)     
+    end
 end
