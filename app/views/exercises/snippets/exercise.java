49846d30f05310ccdc20e027903217d3d7e2d8db
Bill Finn <bfinn@backstopsolutions.com>
2015-09-03 15:47:38 -0500

    Add some more details about the exercise.

--- app/views/exercises/snippets/exercise.java
+++ app/views/exercises/snippets/exercise.java
@@ -1,6 +1,19 @@
+/*
+ * Fibonacci series:
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+ *
+ * FibonacciCalculator calc = new FibonacciCalculator();
+ * assertEqual(0, calc.nthNumber(0));
+ * assertEqual(1, calc.nthNumber(1));
+ ...
+ * assertEqual(8, calc.nthNumber(6));
+ * assertEqual(13, calc.nthNumber(7));
+ * assertEqual(21, calc.nthNumber(8));
+ ...
+ */
 public class FibonacciCalculator {
 
     public Long nthNumber(Integer n) {
-        return 1;
+        return 0;
     }
 }
--- app/views/exercises/snippets/exercise.rb
+++ app/views/exercises/snippets/exercise.rb
@@ -1,2 +1,13 @@
-def nthNumber(n)
+# Fibonacci series:
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+#
+# assertEqual(0, nthFibonacciNumber(0));
+# assertEqual(1, nthFibonacciNumber(1));
+# ...
+# assertEqual(8, nthFibonacciNumber(6));
+# assertEqual(13, nthFibonacciNumber(7));
+# assertEqual(21, nthFibonacciNumber(8));
+# ...
+def nthFibonacciNumber(n)
+  0
 end
